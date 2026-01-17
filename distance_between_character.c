c60404e87b23d21751f6e8ecd71f8f920d8eca8b
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-01-17 22:49:10 +0530

    Distace between two character

---
+++ distance_between_character.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int alpha( int a , int b){
+      
+     return b - a ;
+}
+
+
+
+
+
+int main() {
+    char A , B;
+    scanf("%c %c",&A, &B);
+    int res = alpha(A,B);
+    printf("The distance between %c and %c is %d" , A , B , res);
+
+       
+    return 0;
+}
