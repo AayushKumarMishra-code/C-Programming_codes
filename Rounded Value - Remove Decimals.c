236c3e09e77597a70cfa4d321459192e36acdb14
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-01-28 17:05:16 +0530

    Rounded value

---
+++ Rounded Value - Remove Decimals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float a;
+    scanf("%f", &a);
+    int val = a;
+    printf("Rounded Value = %d", val);
+    return 0;
+}
