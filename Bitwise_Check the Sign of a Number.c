4159bfb91fe521b8d38690bba3fc687f2f863c4a
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-01-29 09:48:03 +0530

    Bitwise_Check the Sign of a Number

---
+++ Bitwise_Check the Sign of a Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+    if (n >> 31 == -1)
+    {
+        printf("Negative");
+    }
+    else
+    {
+        printf("Positive");
+    }
+    return 0;
+}
