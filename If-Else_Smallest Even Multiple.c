72675765b48f26cf4c1b4cc09d53c755b24d2f60
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-02-08 09:28:56 +0530

    Small Even multiple

---
+++ If-Else_Smallest Even Multiple.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    
+    scanf("%d",&n);
+    if(n%2){
+        printf("%d",2*n);
+    }
+    else{
+        printf("%d",n);
+    }
+    return 0;
+}
