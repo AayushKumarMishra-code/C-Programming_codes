df57a211ee6304d7e87a586b26594e7e3e5b2ab7
AayushKumarMishra-code <aayushkumarmishrao6@gmail.com>
2026-03-01 22:05:27 +0530

    first commit

---
+++  Arrays_Find the Minimum Element in an Array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<=n-1;++i){
+        scanf("%d",&a[i]);
+    }
+    int min=a[0];
+    for(int i=1;i<=n-1;++i){
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    printf("%d", min);
+    return 0;
+}
